01e612d625a3be1adc6cc5cf3a0937e30971a979
Daniel Brosnan Blázquez <d.brosnan.b@gmail.com>
2019-04-17 16:50:22 +0200

    Finish first version of the planning view

--- src/s2vboa/views/views.py
+++ src/s2vboa/views/views.py
@@ -1,5 +1,5 @@
 """
-Vies navigation section definition
+Views navigation section definition
 
 Written by DEIMOS Space S.L. (dibb)
 
@@ -22,10 +22,95 @@ bp = Blueprint("views", __name__, url_prefix="/views")
 query = Query()
 engine = Engine()
 
-@bp.route("/planning", methods=["GET"])
-def navigate():
+@bp.route("/planning", methods=["GET", "POST"])
+def show_planning():
     """
-    Initial panel for the EBOA navigation functionality.
+    Planning view for the Sentinel-2 mission.
     """
-    return render_template("views/planning.html")
+    current_app.logger.debug("Planning view")
 
+    planning_events = query_planning_events()
+
+    return render_template("views/planning.html", planning_events=planning_events)
+
+def query_planning_events():
+    """
+    Query planning events.
+    """
+    current_app.logger.debug("Query planning events")
+
+    # Check that the ORBPRE files cover the requested period
+
+    kwargs_imaging = {}
+    kwargs_playback = {}
+
+    if request.method == "POST":
+        if "nppfs" in request.form and request.form["nppfs"] != "":
+            op="notin"
+            if not "nppf_notin_check" in request.form:
+                op="in"
+            # end if
+            kwargs["sources"] = {"filter": [], "op": op}
+            i = 0
+            for nppf in request.form.getlist("nppfs"):
+                kwargs["sources"]["filter"].append(nppf)
+                i+=1
+            # end for
+        # end if
+
+        if request.form["start"] != "":
+            kwargs_imaging["start_filters"] = []
+            kwargs_playback["start_filters"] = []
+            i = 0
+            operators = request.form.getlist("start_operator")
+            for start in request.form.getlist("start"):
+                kwargs_imaging["start_filters"].append({"date": start, "op": operators[i]})
+                kwargs_playback["start_filters"].append({"date": start, "op": operators[i]})
+                i+=1
+            # end for
+        # end if
+        if request.form["stop"] != "":
+            kwargs_imaging["stop_filters"] = []
+            kwargs_playback["stop_filters"] = []
+            i = 0
+            operators = request.form.getlist("stop_operator")
+            for stop in request.form.getlist("stop"):
+                kwargs_imaging["stop_filters"].append({"date": stop, "op": operators[i]})
+                kwargs_playback["stop_filters"].append({"date": stop, "op": operators[i]})
+                i+=1
+            # end for
+        # end if
+        if request.form["ingestion_time"] != "":
+            kwargs_imaging["ingestion_time_filters"] = []
+            kwargs_playback["ingestion_time_filters"] = []
+            i = 0
+            operators = request.form.getlist("ingestion_time_operator")
+            for ingestion_time in request.form.getlist("ingestion_time"):
+                kwargs_imaging["ingestion_time_filters"].append({"date": ingestion_time, "op": operators[i]})
+                kwargs_playback["ingestion_time_filters"].append({"date": ingestion_time, "op": operators[i]})
+                i+=1
+            # end for
+        # end if
+    # end if
+    
+    ####
+    # Query imaging
+    ####
+    # Specify the main query parameters
+    kwargs_imaging["gauge_names"] = {"filter": ["PLANNED_CUT_IMAGING_CORRECTION"], "op": "in"}
+    kwargs_imaging["link_names"] = {"filter": ["TIME_CORRECTION"], "op": "in"}
+    imaging_events = query.get_linked_events(**kwargs_imaging)
+
+    ####
+    # Query playbacks
+    ####
+    # Specify the main query parameters
+    kwargs_playback["gauge_names"] = {"filter": ["PLANNED_PLAYBACK_CORRECTION"], "op": "in"}
+    kwargs_playback["link_names"] = {"filter": ["TIME_CORRECTION"], "op": "in"}
+    playback_events = query.get_linked_events(**kwargs_playback)
+
+    events = {}
+    events["imaging"] = imaging_events
+    events["playback"] = playback_events
+
+    return events
