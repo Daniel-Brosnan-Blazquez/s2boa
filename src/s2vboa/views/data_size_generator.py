1e18c3ef2d36b5eb3f1c779dd1d9ddaade3bdd4b
Daniel Brosnan <d.brosnan.b@gmail.com>
2019-12-05 09:58:43 +0100

    Add reporting generator for the Data Size view

---
+++ src/s2vboa/views/data_size_generator.py
@@ -0,0 +1,39 @@
+"""
+Generator module for the data size view for the monitoring of the Sentinel-2 constellation
+
+Written by DEIMOS Space S.L. (dibb)
+
+module s2vboa
+"""
+# Import python utilities
+import os
+
+# Import helpers
+from vboa.functions import export_html
+
+# Import vboa app creator
+from s2vboa import create_app
+
+version = "1.0"
+
+def generate_report(begin, end, metadata):
+
+    app = create_app()
+    client = app.test_client()
+    response = client.post("/views/data-size", data={
+        "start": begin,
+        "stop": end,
+        "mission": "S2_",
+        "show_data_size_table_details": True,
+        "show_data_size_map": True,
+        "show_data_size_evolution": True
+    })
+
+    html_file_path = export_html(response)
+
+    metadata["operations"][0]["report"]["generator"] = os.path.basename(__file__)
+    metadata["operations"][0]["report"]["generator_version"] = version
+    metadata["operations"][0]["report"]["group"] = "DATA_SIZE"
+    metadata["operations"][0]["report"]["group_description"] = "Group of reports dedicated for the monitoring of the data volume of the Sentinel-2 constellation"
+
+    return html_file_path
