80efa44ce8c197ba0ca60b77272108a787273ab0
Daniel Brosnan <d.brosnan.b@gmail.com>
2020-03-17 17:39:20 +0100

    Correct scheduler and reporting configurations

--- src/s2vboa/views/hktm_workflow_generator.py
+++ src/s2vboa/views/hktm_workflow_generator.py
@@ -1,5 +1,5 @@
 """
-Generator module for the acquisition view for the monitoring of the Sentinel-2 constellation
+Generator module for the hktm workflow view for the monitoring of the Sentinel-2 constellation
 
 Written by DEIMOS Space S.L. (dibb)
 
@@ -16,24 +16,20 @@ from s2vboa import create_app
 
 version = "1.0"
 
-def generate_report(begin, end, metadata):
+def generate_report(begin, end, metadata, parameters = None):
 
     app = create_app()
     client = app.test_client()
-    response = client.post("/views/acquisition", data={
+    response = client.post("/views/hktm-workflow", data={
         "start": begin,
         "stop": end,
-        "mission": "S2_",
-        "show_acquisition_table_details": True,
-        "show_acquisition_map": True,
-        "show_acquisition_timeline": True,
-        "show_station_reports": True
+        "mission": "S2_"
     })
 
     html_file_path = export_html(response)
 
     metadata["operations"][0]["report"]["generator_version"] = version
-    metadata["operations"][0]["report"]["group"] = "ACQUISITION"
-    metadata["operations"][0]["report"]["group_description"] = "Group of reports dedicated for the monitoring of the acquisition chain of the Sentinel-2 constellation"
+    metadata["operations"][0]["report"]["group"] = "HKTM_WORKFLOW"
+    metadata["operations"][0]["report"]["group_description"] = "Group of reports dedicated for the monitoring of the HKTM workflow status of the Sentinel-2 constellation"
 
     return html_file_path
