8acddb4335c60263819b3485c28932ec39205317
dibb <daniel.brosnan@deimos-space.com>
2020-10-30 12:35:45 +0100

    Make generation of PDF available from any working directory

--- doc/tex/generate_doc.sh
+++ doc/tex/generate_doc.sh
@@ -20,13 +20,14 @@ fi
 PDF_FILE=""
 KEEP_BUILD="NO"
 
-while getopts kf: option
+while getopts kf:d: option
 do
     case "${option}"
         in
-        f) PDF_FILE=${OPTARG};;
-        k) KEEP_BUILD="YES";;
-        ?) echo -e $USAGE
+            f) PDF_FILE=${OPTARG};;
+            d) WORKING_DIRECTORY=${OPTARG};;
+            k) KEEP_BUILD="YES";;
+            ?) echo -e $USAGE
             exit -1
     esac
 done
@@ -39,48 +40,72 @@ then
     exit -1
 fi
 
-# Create the build directory
-if [ -d "build" ];
+# Check that option -d has been specified
+if [ "$WORKING_DIRECTORY" == "" ];
 then
-    rm -rf build
+    echo -e "ERROR: The option -d has to be provided"
+    echo -e $USAGE
+    exit -1
 fi
-mkdir build
+
+# Check the working directory exists
+if [ ! -d $WORKING_DIRECTORY ];
+then
+    echo -e "ERROR: The working directory specified does not exist: " $WORKING_DIRECTORY
+    echo -e $USAGE
+    exit -1
+fi
+
+# Check that working directory contains a doc.tex
+if [ ! -f $WORKING_DIRECTORY/doc.tex ];
+then
+    echo -e "ERROR: The working directory does not contain a doc.tex file: " $WORKING_DIRECTORY/doc.tex
+    echo -e $USAGE
+    exit -1
+fi
+
+# Create the build directory
+if [ -d $WORKING_DIRECTORY/build ];
+then
+    rm -rf $WORKING_DIRECTORY/build
+fi
+mkdir $WORKING_DIRECTORY/build
 
 # Automatically Extract the documentation from the python code
 ## S2BOA
 # Generate the tex file
-sphinx-build -b latex ../../src/docs/s2boa/ build
+sphinx-build -b latex $WORKING_DIRECTORY/../../src/docs/s2boa/ $WORKING_DIRECTORY/build
 # Add chapter part
-echo "\chapter{Ingestion modules code documentation}" > s2boa_code_documentation.tex
+echo "\chapter{Ingestion modules code documentation}" > $WORKING_DIRECTORY/s2boa_code_documentation.tex
 # Extract the chapter describing the code
-sed -n '/\\section{Subpackages}/,/\\section{Module contents}/p' build/S2BOA_module.tex |head -n -1 >> s2boa_code_documentation.tex
+sed -n '/\\section{Subpackages}/,/\\section{Module contents}/p' $WORKING_DIRECTORY/build/S2BOA_module.tex |head -n -1 >> $WORKING_DIRECTORY/s2boa_code_documentation.tex
 
 ## S2VBOA
 # Generate the tex file
-sphinx-build -b latex ../../src/docs/s2vboa/ build
+sphinx-build -b latex $WORKING_DIRECTORY/../../src/docs/s2vboa/ $WORKING_DIRECTORY/build
 # Add chapter part
-echo "\chapter{View modules code documentation}" > s2vboa_code_documentation.tex
+echo "\chapter{View modules code documentation}" > $WORKING_DIRECTORY/s2vboa_code_documentation.tex
 # Extract the chapter describing the code
-sed -n '/\\section{Subpackages}/,/\\section{Module contents}/p' build/S2VBOA_module.tex |head -n -1 >> s2vboa_code_documentation.tex
+sed -n '/\\section{Subpackages}/,/\\section{Module contents}/p' $WORKING_DIRECTORY/build/S2VBOA_module.tex |head -n -1 >> $WORKING_DIRECTORY/s2vboa_code_documentation.tex
 
 # Execute the first translation of the tex files into a PDF file
-pdflatex -output-directory build -halt-on-error -interaction=nonstopmode doc.tex &> /dev/null
+TEXINPUTS=".:"$WORKING_DIRECTORY":"$WORKING_DIRECTORY/../fig":" pdflatex -output-directory $WORKING_DIRECTORY/build -halt-on-error -interaction=nonstopmode $WORKING_DIRECTORY/doc.tex &> /dev/null
 
 # Generate glossaries
-makeglossaries -s build/doc.ist build/doc &> /dev/null
+makeglossaries -d $WORKING_DIRECTORY -s build/doc.ist build/doc &> /dev/null
 
 # Execute last translation to generate the PDF with the glossaries
-pdflatex -output-directory build -halt-on-error -interaction=nonstopmode doc.tex |grep -i "warning\|error"
+TEXINPUTS=".:"$WORKING_DIRECTORY":"$WORKING_DIRECTORY/../fig":" pdflatex -output-directory $WORKING_DIRECTORY/build -halt-on-error -interaction=nonstopmode $WORKING_DIRECTORY/doc.tex |grep -i "warning\|error"
 
 # Move generated pdf to its 
-if [ -f build/doc.pdf ];
+if [ -f $WORKING_DIRECTORY/build/doc.pdf ];
 then
-    mv build/doc.pdf $PDF_FILE
+    mv $WORKING_DIRECTORY/build/doc.pdf $WORKING_DIRECTORY/$PDF_FILE
 else
     echo -e "ERROR: There was an error on the generation of the PDF.\nIs pdflatex installed?"
 fi
 
 if [ "$KEEP_BUILD" != "YES" ];
 then
-    rm -rf build
+    rm -rf $WORKING_DIRECTORY/build
 fi
