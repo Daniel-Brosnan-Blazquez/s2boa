061c27afa4d31977f5d45af0821b64e7c6ab0c46
Daniel Brosnan <d.brosnan.b@gmail.com>
2020-11-27 17:55:09 +0100

    Add processing module for TLM_REQ_B files

---
+++ src/s2boa/ingestions/ingestion_tlm_req_b/ingestion_tlm_req_b.py
@@ -0,0 +1,50 @@
+"""
+Ingestion module for the TLM_REQ_B files of Sentinel-2
+
+Written by DEIMOS Space S.L. (dibb)
+
+module eboa
+"""
+# Import python utilities
+import os
+import argparse
+from dateutil import parser
+import datetime
+import json
+
+# Import xml parser
+from lxml import etree
+
+# Import ingestion_functions.helpers
+import eboa.ingestion.functions as ingestion_functions
+import s2boa.ingestions.functions as functions
+
+# Import debugging
+from eboa.debugging import debug
+
+# Import logging
+from eboa.logging import Log
+
+# Import query
+from eboa.engine.query import Query
+
+logging_module = Log(name = __name__)
+logger = logging_module.logger
+
+def process_file(file_path, engine, query, reception_time):
+    """Function to process the file and insert its relevant information
+    into the DDBB of the eboa
+    
+    :param file_path: path to the file to be processed
+    :type file_path: str
+    :param engine: Engine instance
+    :type engine: Engine
+    :param query: Query instance
+    :type query: Query
+    :param reception_time: time of the reception of the file by the triggering
+    :type reception_time: str
+    """
+
+    data = {}
+    
+    return data
---
+++ src/s2boa/ingestions/ingestion_tlm_req_b/tests/test_ingestion_tlm_req_b.py
@@ -0,0 +1,80 @@
+"""
+Automated tests for the ingestion of the TLM_REQ_B files
+
+Written by DEIMOS Space S.L. (dibb)
+
+module s2boa
+"""
+import pdb
+# Import python utilities
+import os
+import sys
+import unittest
+import datetime
+
+# Import engine of the DDBB
+import eboa.engine.engine as eboa_engine
+from eboa.engine.engine import Engine
+from eboa.engine.query import Query
+from eboa.datamodel.base import Session, engine, Base
+from eboa.engine.errors import LinksInconsistency, UndefinedEventLink, DuplicatedEventLinkRef, WrongPeriod, SourceAlreadyIngested, WrongValue, OddNumberOfCoordinates, EboaResourcesPathNotAvailable, WrongGeometry, ErrorParsingDictionary
+from eboa.engine.query import Query
+
+# Import datamodel
+from eboa.datamodel.dim_signatures import DimSignature
+from eboa.datamodel.events import Event, EventLink, EventKey, EventText, EventDouble, EventObject, EventGeometry, EventBoolean, EventTimestamp
+from eboa.datamodel.gauges import Gauge
+from eboa.datamodel.sources import Source, SourceStatus
+from eboa.datamodel.explicit_refs import ExplicitRef, ExplicitRefGrp, ExplicitRefLink
+from eboa.datamodel.annotations import Annotation, AnnotationCnf, AnnotationText, AnnotationDouble, AnnotationObject, AnnotationGeometry, AnnotationBoolean, AnnotationTimestamp
+
+# Import ingestion
+import eboa.ingestion.eboa_ingestion as ingestion
+
+class TestTlmReqB(unittest.TestCase):
+    def setUp(self):
+        # Create the engine to manage the data
+        self.engine_eboa = Engine()
+        self.query_eboa = Query()
+
+        # Create session to connectx to the database
+        self.session = Session()
+
+        # Clear all tables before executing the test
+        self.query_eboa.clear_db()
+
+    def tearDown(self):
+        # Close connections to the DDBB
+        self.engine_eboa.close_session()
+        self.query_eboa.close_session()
+        self.session.close()
+
+    def test_insert_tlm_req(self):
+        filename = "S2B_OPER_TLM__REQ_B_20201126T000000_20201127T000000_0001.EOF"
+        file_path = os.path.dirname(os.path.abspath(__file__)) + "/inputs/" + filename
+
+        returned_value = ingestion.command_process_file("s2boa.ingestions.ingestion_tlm_req_b.ingestion_tlm_req_b", file_path, "2018-01-01T00:00:00")
+
+        assert returned_value[0]["status"] == eboa_engine.exit_codes["OK"]["status"]
+
+    def test_insert_tlm_req_with_plan_and_orbpre(self):
+        filename = "S2B_OPER_MPL__NPPF__20201112T120000_20201130T150000_0001.EOF"
+        file_path = os.path.dirname(os.path.abspath(__file__)) + "/inputs/" + filename
+
+        returned_value = ingestion.command_process_file("s2boa.ingestions.ingestion_nppf.ingestion_nppf", file_path, "2018-01-01T00:00:00")
+
+        assert returned_value[0]["status"] == eboa_engine.exit_codes["OK"]["status"]
+
+        filename = "S2B_OPER_MPL_ORBPRE_20201125T030111_20201205T030111_0001.EOF"
+        file_path = os.path.dirname(os.path.abspath(__file__)) + "/inputs/" + filename
+
+        returned_value = ingestion.command_process_file("s2boa.ingestions.ingestion_orbpre.ingestion_orbpre", file_path, "2018-01-01T00:00:00")
+
+        assert returned_value[0]["status"] == eboa_engine.exit_codes["OK"]["status"]
+
+        filename = "S2B_OPER_TLM__REQ_B_20201126T000000_20201127T000000_0001.EOF"
+        file_path = os.path.dirname(os.path.abspath(__file__)) + "/inputs/" + filename
+
+        returned_value = ingestion.command_process_file("s2boa.ingestions.ingestion_tlm_req_b.ingestion_tlm_req_b", file_path, "2018-01-01T00:00:00")
+
+        assert returned_value[0]["status"] == eboa_engine.exit_codes["OK"]["status"]
