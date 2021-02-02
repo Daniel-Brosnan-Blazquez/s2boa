6177db0a324d7e2644b74ea01c9e62d2eb456654
JUBV <john.bradley@elecnor.es>
2021-02-02 14:33:19 +0100

    Fix values for the SAD coverage and datastrip fields in tables and tooltips

--- src/s2vboa/templates/js/processing/incomplete_processing_to_timeline.js
+++ src/s2vboa/templates/js/processing/incomplete_processing_to_timeline.js
@@ -9,15 +9,16 @@ var incomplete_processing_timeline = [
     {% set level = event.eventTexts|selectattr("name", "equalto", "level")|map(attribute='value')|first|string %}
     {% set sensing_orbit = event.eventDoubles|selectattr("name", "equalto", "sensing_orbit")|map(attribute='value')|first|int %}
     {% set status = event.eventTexts|selectattr("name", "equalto", "status")|map(attribute='value')|first|string %}
-    {% set datastrip = original_isp_validity.event_uuid %}
-    {% set sad_data = original_isp_validity.eventLinks|selectattr("name", "equalto", "SAD_DATA")|map(attribute='event_uuid_link')|list|first %}
+    {% set datastrip_er = event.explicitRef.explicit_ref %}
+    {% set sad_data_uuids = original_isp_validity.eventLinks|selectattr("name", "equalto", "SAD_DATA")|map(attribute='event_uuid_link')|list %}
+    {% set sad_data = processing_events["sad_data"]|selectattr("event_uuid", "in", sad_data_uuids)|first %}
     {
         "id": "{{ event.event_uuid }}",
         "group": "{{ satellite }}",
         "timeline": "{{ level }}",
         "start": "{{ event.start.isoformat() }}",
         "stop": "{{ event.stop.isoformat() }}",
-        "tooltip": create_processing_tooltip_text("{{ event.event_uuid }}", "{{ satellite }}", "{{ downlink_orbit }}", "{{ station }}", "{{ level }}", "{{ sensing_orbit }}", "<a class=bold-orange>{{ status }}</a>", "/eboa_nav/query-event-links/{{ sad_data }}", "/eboa_nav/query-event-links/{{ datastrip }}"),
+        "tooltip": create_processing_tooltip_text("{{ event.event_uuid }}", "{{ satellite }}", "{{ downlink_orbit }}", "{{ station }}", "{{ level }}", "{{ sensing_orbit }}", "<a class=bold-green>{{ status }}</a>", "<a href='/eboa_nav/query-event-links/{{ event.event_uuid }}'>{{ datastrip_er }}</a>", "<a href='/eboa_nav/query-event-links/{{ sad_data.event_uuid }}'>{{ sad_data.start ~ "_" ~ sad_data.stop }}</a>"),
         "className": "fill-border-orange"
     },
     {% endfor %}
--- src/s2vboa/templates/js/processing/processing_functions.js
+++ src/s2vboa/templates/js/processing/processing_functions.js
@@ -1,5 +1,5 @@
 /* Function to create the text for the tooltip of the datastrip event information*/
-function create_processing_tooltip_text(uuid, satellite, downlink_orbit, station, level, sensing_orbit, status, sad_data, link_to_details){
+function create_processing_tooltip_text(uuid, satellite, downlink_orbit, station, level, sensing_orbit, status, datastrip_er, sad_data){
     
     return "<table border='1'>" +
             "<tr><td>UUID</td><td>" + uuid + "</td></tr>" +
@@ -9,7 +9,7 @@ function create_processing_tooltip_text(uuid, satellite, downlink_orbit, station
             "<tr><td>Level</td><td>" + level + "</td></tr>" +
             "<tr><td>Sensing orbit</td><td>" + sensing_orbit + "</td></tr>" +
             "<tr><td>Status</td><td>" + status + "</td></tr>" +
-            '<tr><td>SAD data</td><td><a href="' + sad_data + '"><i class="fa fa-link"></i></a></td></tr>' +
-            '<tr><td>Details</td><td><a href="' + link_to_details + '"><i class="fa fa-link"></i></a></td></tr>' +
+            '<tr><td>Datastrip</td><td>' + datastrip_er + '</td></tr>' +
+            '<tr><td>SAD coverage</td><td>' + sad_data + '</td></tr>' +
             "</table>"
 };
