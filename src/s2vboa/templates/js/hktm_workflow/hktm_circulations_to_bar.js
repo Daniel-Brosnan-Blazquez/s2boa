2bb1e1abe49ad28fbbe1e6c66933f06b3022eea3
John Bradley Valdenebro <john.bradley@elecnor.es>
2021-02-17 14:21:43 +0100

    Add status and completeness status fields to the tooltips in the hktm workflow view

--- src/s2vboa/templates/js/hktm_workflow/hktm_circulations_to_bar.js
+++ src/s2vboa/templates/js/hktm_workflow/hktm_circulations_to_bar.js
@@ -8,6 +8,13 @@ var hktm_circulation_events = [
     {% set station = event.eventTexts|selectattr("name", "equalto", "station")|map(attribute='value')|first|string %}
     {% set hktm_production_event_uuids = event.eventLinks|selectattr("name", "equalto", "HKTM_PRODUCTION_VGS")|map(attribute='event_uuid_link')|list %}
 
+    {% set status = [] %}
+    {% set status_class = [] %}     	 
+    {% include "views/hktm_workflow/hktm_workflow_status.html" %}
+    {% set completeness_status = [] %}
+    {% set completeness_status_class = [] %}     	 
+    {% include "views/hktm_workflow/hktm_workflow_completeness_status.html" %}
+    
     {% if hktm_production_event_uuids|length > 0 %}
     {% set hktm_production_events = hktm_workflow_events["hktm_production_vgs"]|selectattr("event_uuid", "in", hktm_production_event_uuids)|list %}
 
@@ -35,7 +42,7 @@ var hktm_circulation_events = [
         "group": "{{ satellite }}",
         "x": "{{ orbpre_event.start.isoformat() }}",
         "y": "{{ delta_to_fos }}",
-        "tooltip": create_hktm_workflow_tooltip_text("<a href='/eboa_nav/query-er/{{ hktm_production_event.explicit_ref_uuid }}'>{{ hktm_production_event.explicitRef.explicit_ref }}</a>", "{{ satellite }}", "<a href='/eboa_nav/query-event-links/{{ event.event_uuid }}'>{{ orbit }}</a>", "{{ station }}", "{{ orbpre_event.start.isoformat() }}", "{{ circulation_time_to_fos }}", "<span class='{{ delta_to_fos_class }}'>{{ delta_to_fos }}</span>", "{{ product_size_to_fos }}")
+        "tooltip": create_hktm_workflow_tooltip_text("<a href='/eboa_nav/query-er/{{ hktm_production_event.explicit_ref_uuid }}'>{{ hktm_production_event.explicitRef.explicit_ref }}</a>", "{{ satellite }}", "<a href='/eboa_nav/query-event-links/{{ event.event_uuid }}'>{{ orbit }}</a>", "{{ station }}", "<span class='{{ status_class[0] }}'>{{ status[0] }}</span>", "<span class='{{ completeness_status_class[0] }}'>{{ completeness_status[0] }}</span>","{{ orbpre_event.start.isoformat() }}", "{{ circulation_time_to_fos }}", "<span class='{{ delta_to_fos_class }}'>{{ delta_to_fos }}</span>", "{{ product_size_to_fos }}")
     },
     {% endif %}
     {% endif %}
--- src/s2vboa/templates/js/hktm_workflow/hktm_workflow_functions.js
+++ src/s2vboa/templates/js/hktm_workflow/hktm_workflow_functions.js
@@ -1,11 +1,13 @@
 /* Function to create the text for the tooltip of the dissemination information*/
-function create_hktm_workflow_tooltip_text(hktm_product, satellite, orbit, station, anx_time, pdmc_fos_time, delta_to_fos, product_size){
+function create_hktm_workflow_tooltip_text(hktm_product, satellite, orbit, station, status, completeness_status, anx_time, pdmc_fos_time, delta_to_fos, product_size){
     
     return "<table border='1'>" +
             "<tr><td>HKTM Product</td><td>" + hktm_product + "</td></tr>" +
             "<tr><td>Satellite</td><td>" + satellite + "</td></tr>" +
             "<tr><td>Orbit</td><td>" + orbit + "</td></tr>" +
             "<tr><td>Station</td><td>" + station + "</td></tr>" +
+            "<tr><td>Status</td><td>" + status + "</td></tr>" +
+            "<tr><td>Completeness status</td><td>" + completeness_status + "</td></tr>" +
             "<tr><td>ANX time</td><td>" + anx_time + "</td></tr>" +
             "<tr><td>PDMC-FOS time</td><td>" + pdmc_fos_time + "</td></tr>" +
             "<tr><td>Delta to FOS (m)</td><td>" + delta_to_fos + "</td></tr>" +
