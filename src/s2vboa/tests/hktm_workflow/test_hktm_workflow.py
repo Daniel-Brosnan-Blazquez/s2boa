fe1702e04d7cf77ea33bfdcb49540a913534e956
dibb <daniel.brosnan@deimos-space.com>
2021-02-18 10:15:14 +0100

    Add test for checking a correct circulation of HKTM and a missing production case

--- src/s2vboa/tests/hktm_workflow/test_hktm_workflow.py
+++ src/s2vboa/tests/hktm_workflow/test_hktm_workflow.py
@@ -317,7 +317,7 @@ class TestHktmWorkflowView(unittest.TestCase):
         assert summary_missing_production.text == "1"
 
         # Check summary missing production, data received from PDGS
-        summary_missing_production_data_received_from_pdgs = wait.until(EC.visibility_of_element_located((By.ID,"summary-hktm-production-data-received")))
+        summary_missing_production_data_received_from_pdgs = wait.until(EC.visibility_of_element_located((By.ID,"summary-missing-hktm-production-data-received")))
 
         assert summary_missing_production_data_received_from_pdgs
 
@@ -1026,3 +1026,280 @@ class TestHktmWorkflowView(unittest.TestCase):
 
         assert comments.text == ""
 
+    def test_hktm_workflow_with_missing(self):
+
+        filename = "S2A_NPPF.EOF"
+        file_path = os.path.dirname(os.path.abspath(__file__)) + "/inputs/" + filename
+
+        exit_status = ingestion.command_process_file("s2boa.ingestions.ingestion_nppf.ingestion_nppf", file_path, "2018-01-01T00:00:00")
+
+        assert len([item for item in exit_status if item["status"] != eboa_engine.exit_codes["OK"]["status"]]) == 0
+
+        filename = "S2A_ORBPRE.EOF"
+        file_path = os.path.dirname(os.path.abspath(__file__)) + "/inputs/" + filename
+
+        exit_status = ingestion.command_process_file("s2boa.ingestions.ingestion_orbpre.ingestion_orbpre", file_path, "2018-01-01T00:00:00")
+
+        assert len([item for item in exit_status if item["status"] != eboa_engine.exit_codes["OK"]["status"]]) == 0
+
+        filename = "S2A_OPER_MPL_SPSGS__PDMC_20200128T090002_V20200129T000000_20200129T060000.EOF"
+        file_path = os.path.dirname(os.path.abspath(__file__)) + "/inputs/" + filename
+
+        exit_status = ingestion.command_process_file("s2boa.ingestions.ingestion_station_schedule.ingestion_station_schedule", file_path, "2018-01-01T00:00:00")
+
+        assert len([item for item in exit_status if item["status"] != eboa_engine.exit_codes["OK"]["status"]]) == 0
+        
+        filename = "S2A_REP_PASS.EOF"
+        file_path = os.path.dirname(os.path.abspath(__file__)) + "/inputs/" + filename
+
+        exit_status = ingestion.command_process_file("s2boa.ingestions.ingestion_dfep_acquisition.ingestion_dfep_acquisition", file_path, "2018-01-01T00:00:00")
+
+        assert len([item for item in exit_status if item["status"] != eboa_engine.exit_codes["OK"]["status"]]) == 0
+
+        filename = "S2A_OPER_REP_OPHKTM_VGS2_20200129T034000_V20200129T032508_20200129T032513.EOF"
+        file_path = os.path.dirname(os.path.abspath(__file__)) + "/inputs/" + filename
+
+        exit_status = ingestion.command_process_file("s2boa.ingestions.ingestion_ophktm.ingestion_ophktm", file_path, "2018-01-01T00:00:00")
+
+        assert len([item for item in exit_status if item["status"] != eboa_engine.exit_codes["OK"]["status"]]) == 0
+        
+        filename = "S2__REP_OPDC.EOF"
+        file_path = os.path.dirname(os.path.abspath(__file__)) + "/inputs/" + filename
+
+        exit_status = ingestion.command_process_file("s2boa.ingestions.ingestion_dc.ingestion_dc", file_path, "2018-01-01T00:00:00")
+
+        assert len([item for item in exit_status if item["status"] != eboa_engine.exit_codes["OK"]["status"]]) == 0
+
+        filename = "S2A_NPPF_ONLY_HKTM.EOF"
+        file_path = os.path.dirname(os.path.abspath(__file__)) + "/inputs/" + filename
+
+        exit_status = ingestion.command_process_file("s2boa.ingestions.ingestion_nppf.ingestion_nppf", file_path, "2018-01-01T00:00:00")
+
+        assert len([item for item in exit_status if item["status"] != eboa_engine.exit_codes["OK"]["status"]]) == 0
+
+        filename = "S2A_ORBPRE_ONLY_HKTM.EOF"
+        file_path = os.path.dirname(os.path.abspath(__file__)) + "/inputs/" + filename
+
+        exit_status = ingestion.command_process_file("s2boa.ingestions.ingestion_orbpre.ingestion_orbpre", file_path, "2018-01-01T00:00:00")
+
+        assert len([item for item in exit_status if item["status"] != eboa_engine.exit_codes["OK"]["status"]]) == 0
+
+        filename = "S2A_REP_PASS_ONLY_HKTM.EOF"
+        file_path = os.path.dirname(os.path.abspath(__file__)) + "/inputs/" + filename
+
+        exit_status = ingestion.command_process_file("s2boa.ingestions.ingestion_dfep_acquisition.ingestion_dfep_acquisition", file_path, "2018-01-01T00:00:00")
+
+        assert len([item for item in exit_status if item["status"] != eboa_engine.exit_codes["OK"]["status"]]) == 0
+
+        wait = WebDriverWait(self.driver,5)
+
+        self.driver.get("http://localhost:5000/views/hktm-workflow")
+
+        functions.query(self.driver, wait, "S2A", start = "2014-07-20T00:00:14", stop = "2020-07-21T23:55:14", start_orbit = "16066", stop_orbit = "16072", timeline = True, table_details = True, evolution = True, map = True)
+
+        # Check summary expected
+        summary_expected = wait.until(EC.visibility_of_element_located((By.ID,"summary-hktm-workflow-expected")))
+
+        assert summary_expected
+
+        assert summary_expected.text == "2"
+
+        # Check summary generated
+        summary_generated = wait.until(EC.visibility_of_element_located((By.ID,"summary-hktm-workflow-generated")))
+
+        assert summary_generated
+
+        assert summary_generated.text == "1"
+
+        # Check summary generated
+        summary_missing = wait.until(EC.visibility_of_element_located((By.ID,"summary-hktm-workflow-missing-production")))
+
+        assert summary_missing
+
+        assert summary_missing.text == "1"
+
+        # Check summary generated
+        summary_missing_data_received_by_pdgs = wait.until(EC.visibility_of_element_located((By.ID,"summary-missing-hktm-production-data-received")))
+
+        assert summary_missing_data_received_by_pdgs
+
+        assert summary_missing_data_received_by_pdgs.text == "1"
+
+        # Check number of elements in summary
+        assert len(summary_expected.find_elements_by_xpath("../../../div")) == 4
+
+        # General table
+        general_table = self.driver.find_element_by_id("hktm-workflow-list-hktm-table")
+
+        satellite = general_table.find_element_by_xpath("tbody/tr[1]/td[1]")
+
+        assert satellite.text == "S2A"
+
+        orbit = general_table.find_element_by_xpath("tbody/tr[1]/td[2]")
+
+        assert orbit.text == "24040"
+
+        station = general_table.find_element_by_xpath("tbody/tr[1]/td[3]")
+
+        assert station.text == "N/A"
+
+        anx_time = general_table.find_element_by_xpath("tbody/tr[1]/td[4]")
+
+        assert anx_time.text == "2020-01-29T04:38:33.357330"
+
+        status = general_table.find_element_by_xpath("tbody/tr[1]/td[5]")
+
+        assert status.text == "HKTM PLAYBACK NOT PLANNED"
+
+        completeness_status = general_table.find_element_by_xpath("tbody/tr[1]/td[6]")
+
+        assert completeness_status.text == "HKTM PLAYBACK NOT PLANNED"
+
+        hktm_product = general_table.find_element_by_xpath("tbody/tr[1]/td[7]")
+
+        assert hktm_product.text == "N/A"
+
+        pdmc_fos_time = general_table.find_element_by_xpath("tbody/tr[1]/td[8]")
+
+        assert pdmc_fos_time.text == "N/A"
+
+        time_fos = general_table.find_element_by_xpath("tbody/tr[1]/td[9]")
+
+        assert time_fos.text == "N/A"
+
+        comments = general_table.find_element_by_xpath("tbody/tr[1]/td[10]")
+
+        assert comments.text == ""
+
+        satellite = general_table.find_element_by_xpath("tbody/tr[2]/td[1]")
+
+        assert satellite.text == "S2A"
+
+        orbit = general_table.find_element_by_xpath("tbody/tr[2]/td[2]")
+
+        assert orbit.text == "24039"
+
+        station = general_table.find_element_by_xpath("tbody/tr[2]/td[3]")
+
+        assert station.text == "SGS_"
+
+        anx_time = general_table.find_element_by_xpath("tbody/tr[2]/td[4]")
+
+        assert anx_time.text == "2020-01-29T02:57:51.366847"
+
+        status = general_table.find_element_by_xpath("tbody/tr[2]/td[5]")
+
+        assert status.text == "OK"
+
+        completeness_status = general_table.find_element_by_xpath("tbody/tr[2]/td[6]")
+
+        assert completeness_status.text == "OK"
+
+        hktm_product = general_table.find_element_by_xpath("tbody/tr[2]/td[7]")
+
+        assert hktm_product.text == "S2A_OPER_PRD_HKTM___20200129T032508_20200129T032513_0001"
+
+        pdmc_fos_time = general_table.find_element_by_xpath("tbody/tr[2]/td[8]")
+
+        assert pdmc_fos_time.text == "2020-01-29T03:29:21"
+
+        time_fos = general_table.find_element_by_xpath("tbody/tr[2]/td[9]")
+
+        assert time_fos.text == "31.494"
+
+        comments = general_table.find_element_by_xpath("tbody/tr[2]/td[10]")
+
+        assert comments.text == ""
+
+        satellite = general_table.find_element_by_xpath("tbody/tr[4]/td[1]")
+
+        assert satellite.text == "S2A"
+
+        orbit = general_table.find_element_by_xpath("tbody/tr[4]/td[2]")
+
+        assert orbit.text == "16073"
+
+        station = general_table.find_element_by_xpath("tbody/tr[4]/td[3]")
+
+        assert station.text == "N/A"
+
+        anx_time = general_table.find_element_by_xpath("tbody/tr[4]/td[4]")
+
+        assert anx_time.text == "2018-07-21T01:27:21.897531"
+
+        status = general_table.find_element_by_xpath("tbody/tr[4]/td[5]")
+
+        assert status.text == "MISSING PRODUCTION"
+
+        completeness_status = general_table.find_element_by_xpath("tbody/tr[4]/td[6]")
+
+        assert completeness_status.text == "OK"
+
+        hktm_product = general_table.find_element_by_xpath("tbody/tr[4]/td[7]")
+
+        assert hktm_product.text == "N/A"
+
+        pdmc_fos_time = general_table.find_element_by_xpath("tbody/tr[4]/td[8]")
+
+        assert pdmc_fos_time.text == "N/A"
+
+        time_fos = general_table.find_element_by_xpath("tbody/tr[4]/td[9]")
+
+        assert time_fos.text == "N/A"
+
+        comments = general_table.find_element_by_xpath("tbody/tr[4]/td[10]")
+
+        assert comments.text == ""
+
+        planned_playback = self.query_eboa.get_events(gauge_names ={"filter": "PLANNED_PLAYBACK", "op":"=="})
+        hktm_production_vgs = self.query_eboa.get_events(gauge_names ={"filter": "HKTM_PRODUCTION_VGS", "op":"=="})
+        
+        # HKTM circulation info
+        hktm_circulation_info = [
+            {
+                "id": str(hktm_production_vgs[0].event_uuid),
+                "group": "S2A",
+                "x": "2020-01-29T02:57:51.366847",
+                "y": "31.494",
+                "tooltip": "<table border='1'>" +
+                "<tr><td>HKTM Product</td><td><a href='/eboa_nav/query-er/" + str(hktm_production_vgs[0].explicitRef.explicit_ref_uuid) + "'>S2A_OPER_PRD_HKTM___20200129T032508_20200129T032513_0001</a></td></tr>" +
+                "<tr><td>Satellite</td><td>S2A</td></tr>" +
+                "<tr><td>Orbit</td><td><a href='/eboa_nav/query-event-links/" + str(planned_playback[0].event_uuid) + "'>24039</a></td></tr>" +
+                "<tr><td>Station</td><td>SGS_</td></tr>" +
+                "<tr><td>Status</td><td><span class='bold-green'>OK</span></td></tr>" +
+                "<tr><td>Completeness status</td><td><span class='bold-green'>OK</span></td></tr>" +
+                "<tr><td>ANX time</td><td>2020-01-29T02:57:51.366847</td></tr>" +
+                "<tr><td>PDMC-FOS time</td><td>2020-01-29T03:29:21</td></tr>" +
+                "<tr><td>Delta to FOS (m)</td><td><span class='bold-green'>31.494</span></td></tr>" +
+                "<tr><td>Product size (B)</td><td>38338560.0</td></tr>" +
+                "</table>"
+            },
+        ]
+        
+        returned_htkm_circulation_info = self.driver.execute_script('return hktm_circulation_events;')
+        assert hktm_circulation_info == returned_htkm_circulation_info
+
+        # HKTM size info
+        hktm_size_info = [
+            {
+                "id": str(hktm_production_vgs[0].event_uuid),
+                "group": "S2A",
+                "x": "2020-01-29T02:57:51.366847",
+                "y": "38338560.0",
+                "tooltip": "<table border='1'>" +
+                "<tr><td>HKTM Product</td><td><a href='/eboa_nav/query-er/" + str(hktm_production_vgs[0].explicitRef.explicit_ref_uuid) + "'>S2A_OPER_PRD_HKTM___20200129T032508_20200129T032513_0001</a></td></tr>" +
+                "<tr><td>Satellite</td><td>S2A</td></tr>" +
+                "<tr><td>Orbit</td><td><a href='/eboa_nav/query-event-links/" + str(planned_playback[0].event_uuid) + "'>24039</a></td></tr>" +
+                "<tr><td>Station</td><td>SGS_</td></tr>" +
+                "<tr><td>Status</td><td><span class='bold-green'>OK</span></td></tr>" +
+                "<tr><td>Completeness status</td><td><span class='bold-green'>OK</span></td></tr>" +
+                "<tr><td>ANX time</td><td>2020-01-29T02:57:51.366847</td></tr>" +
+                "<tr><td>PDMC-FOS time</td><td>2020-01-29T03:29:21</td></tr>" +
+                "<tr><td>Delta to FOS (m)</td><td><span class='bold-green'>31.494</span></td></tr>" +
+                "<tr><td>Product size (B)</td><td>38338560.0</td></tr>" +
+                "</table>"
+            },
+        ]
+        
+        returned_htkm_size_info = self.driver.execute_script('return hktm_size_events;')
+        assert hktm_size_info == returned_htkm_size_info
