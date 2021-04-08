39ff2325aaa3042783df228b8ee2fe2c5db32bc8
dibb <daniel.brosnan@deimos-space.com>
2021-04-08 10:57:55 +0200

    Add dependency with eboa

--- src/setup.py
+++ src/setup.py
@@ -1,22 +1,22 @@
 """
-Setup configuration for the vboa application
+Setup configuration for the s2boa application
 
 Written by DEIMOS Space S.L. (dibb)
 
-module vboa
+module s2boa
 """
 from setuptools import setup, find_packages
 
 setup(name="s2boa",
       version="1.0.0",
       description="S2 engine and visualization tool for Business Operation Analysis",
-      url="https://bitbucket.org/dbrosnan/s2boa/",
       author="Daniel Brosnan",
       author_email="daniel.brosnan@deimos-space.com",
       packages=find_packages(),
       include_package_data=True,
       python_requires='>=3',
       install_requires=[
+          "eboa",
           "vboa",
           "astropy",
           "massedit"
