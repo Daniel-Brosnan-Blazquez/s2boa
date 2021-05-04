4ea744f815fd022b730758d444b0471ed300e5a4
Miguel Agudo <miguel.agudo@elecnor.es>
2021-05-04 11:43:31 +0200

    Update of the REP_SUP ingestion module

--- src/s2boa/ingestions/ingestion_sup/test/test_ingestion_sup.py
+++ src/s2boa/ingestions/ingestion_sup/test/test_ingestion_sup.py
@@ -81,7 +81,6 @@ class TestOpprip(unittest.TestCase):
                                                     sources = {"filter": "S2A_OPER_REP__SUP___20201128T134025_99999999T999999_0001.EOF", "op": "=="},
                                                     start_filters = [{"date": "2020-11-28T13:40:25", "op": "=="}])
         
-
         assert len(sup_event) == 1
 
         assert sup_events[0].get_structured_values() == [
@@ -101,9 +100,10 @@ class TestOpprip(unittest.TestCase):
                     "type": "double",
                     "value": "28392.0"
                     }]
-    
+
     # Check if the previously inserted event is updated
     def test_insert_repsups_with_same_key(self):
+
         filename = "S2A_OPER_REP__SUP___20201128T134025_99999999T999999_0001.EOF"
         file_path = os.path.dirname(os.path.abspath(__file__)) + "/inputs/" + filename
 
@@ -168,15 +168,12 @@ class TestOpprip(unittest.TestCase):
     # Check links
     def test_insert_repsups_with_links(self):
         
-        self.query_eboa.clear_db()
-        
         filename = "S2B_OPER_MPL__NPPF__20201112T120000_20201130T150000_0001.EOF"
         file_path = os.path.dirname(os.path.abspath(__file__)) + "/inputs/" + filename
 
         exit_status = ingestion.command_process_file("s2boa.ingestions.ingestion_nppf.ingestion_nppf", file_path, "2018-01-01T00:00:00")
 
         assert len([item for item in exit_status if item["status"] != eboa_engine.exit_codes["OK"]["status"]]) == 0
-        
 
         filename = "S2B_OPER_MPL_ORBPRE_20201125T030111_20201205T030111_0001.EOF"
         file_path = os.path.dirname(os.path.abspath(__file__)) + "/inputs/" + filename
@@ -184,6 +181,7 @@ class TestOpprip(unittest.TestCase):
         exit_status = ingestion.command_process_file("s2boa.ingestions.ingestion_orbpre.ingestion_orbpre", file_path, "2018-01-01T00:00:00")
 
         assert len([item for item in exit_status if item["status"] != eboa_engine.exit_codes["OK"]["status"]]) == 0
+
 
         filename = "S2B_OPER_REP__SUP___20201126T130000_20201127T103000_0001.test"
         file_path = os.path.dirname(os.path.abspath(__file__)) + "/inputs/" + filename
@@ -194,14 +192,135 @@ class TestOpprip(unittest.TestCase):
 
         sources = self.query_eboa.get_sources()
 
-        assert len(sources) == 6
-
         # Number of rep_sup events
-        sup_event = self.query_eboa.get_events(gauge_names = {"filter": "SYSTEM_UNAVAILABILITY", "op": "=="},
+        sup_events = self.query_eboa.get_events(gauge_names = {"filter": "SYSTEM_UNAVAILABILITY", "op": "=="},
                                                     gauge_systems = {"filter": "S2B", "op": "=="},
                                                     sources = {"filter": "S2B_OPER_REP__SUP___20201126T130000_20201127T103000_0001.test", "op": "=="})
         
-        assert len(sup_event) == 4
+        assert len(sup_events) == 4
+
+        
+
+        # Values of the different inserted events
+        # XBAND event
+        xband_sup_event = self.query_eboa.get_events(gauge_names = {"filter": "SYSTEM_UNAVAILABILITY", "op": "=="},
+                                                    gauge_systems = {"filter": "S2B", "op": "=="},
+                                                    sources = {"filter": "S2B_OPER_REP__SUP___20201126T130000_20201127T103000_0001.test", "op": "=="},
+                                                    start_filters = [{"date": "2020-11-26T13:00:00", "op": "=="}])
+
+        assert len(xband_sup_event) == 1
+
+        assert xband_sup_event[0].get_structured_values() == [
+                    {"name": "satellite",
+                    "type": "text",
+                    "value": "S2B"
+                    },
+                    {"name": "subsystem",
+                    "type": "text",
+                    "value": "X-BAND TX-1A"
+                    },
+                    {"name": "comment",
+                    "type": "text",
+                    "value": "X-BAND inpamcted due to a debris collision"
+                    },
+                    {"name": "start_orbit",
+                    "type": "double",
+                    "value": "19450.0"
+                    }, 
+                    {"name": "stop_orbit",
+                    "type": "double",
+                    "value": "19450.0"
+                    }]
+
+        # OCP event
+        ocp_sup_event = self.query_eboa.get_events(gauge_names = {"filter": "SYSTEM_UNAVAILABILITY", "op": "=="},
+                                                    gauge_systems = {"filter": "S2B", "op": "=="},
+                                                    sources = {"filter": "S2B_OPER_REP__SUP___20201126T130000_20201127T103000_0001.test", "op": "=="},
+                                                    start_filters = [{"date": "2020-11-26T22:45:00", "op": "=="}])
+    
+        assert len(ocp_sup_event) == 1
+
+        assert ocp_sup_event[0].get_structured_values() == [
+                    {"name": "satellite",
+                    "type": "text",
+                    "value": "S2B"
+                    },
+                    {"name": "subsystem",
+                    "type": "text",
+                    "value": "OCP"
+                    },
+                    {"name": "comment",
+                    "type": "text",
+                    "value": "Outage due to GS2_OCP-04 TAPCO RESET Outage due to GS2_OCP-04 TAPCO RESET"
+                    },
+                    {"name": "start_orbit",
+                    "type": "double",
+                    "value": "19451.0"
+                    }, 
+                    {"name": "stop_orbit",
+                    "type": "double",
+                    "value": "19451.0"
+                    }]
+
+        # MSI event
+        msi_sup_event = self.query_eboa.get_events(gauge_names = {"filter": "SYSTEM_UNAVAILABILITY", "op": "=="},
+                                                    gauge_systems = {"filter": "S2B", "op": "=="},
+                                                    sources = {"filter": "S2B_OPER_REP__SUP___20201126T130000_20201127T103000_0001.test", "op": "=="},
+                                                    start_filters = [{"date": "2020-11-26T15:00:00", "op": "=="}])
+        
+        assert len(msi_sup_event) == 1
+
+        assert msi_sup_event[0].get_structured_values() == [
+                    {"name": "satellite",
+                    "type": "text",
+                    "value": "S2B"
+                    },
+                    {"name": "subsystem",
+                    "type": "text",
+                    "value": "MSI"
+                    },
+                    {"name": "comment",
+                    "type": "text",
+                    "value": "Laser failure"
+                    },
+                    {"name": "start_orbit",
+                    "type": "double",
+                    "value": "19450.0"
+                    }, 
+                    {"name": "stop_orbit",
+                    "type": "double",
+                    "value": "19451.0"
+                    }]
+
+        # MMFU event
+        mmfu_sup_event = self.query_eboa.get_events(gauge_names = {"filter": "SYSTEM_UNAVAILABILITY", "op": "=="},
+                                                    gauge_systems = {"filter": "S2B", "op": "=="},
+                                                    sources = {"filter": "S2B_OPER_REP__SUP___20201126T130000_20201127T103000_0001.test", "op": "=="},
+                                                    start_filters = [{"date": "2020-11-26T19:00:00", "op": "=="}])
+        
+        assert len(mmfu_sup_event) == 1
+
+        assert mmfu_sup_event[0].get_structured_values() == [
+                    {"name": "satellite",
+                    "type": "text",
+                    "value": "S2B"
+                    },
+                    {"name": "subsystem",
+                    "type": "text",
+                    "value": "MMFU-A"
+                    },
+                    {"name": "comment",
+                    "type": "text",
+                    "value": "MMFU impacted due to a memory full on board"
+                    },
+                    {"name": "start_orbit",
+                    "type": "double",
+                    "value": "19451.0"
+                    }, 
+                    {"name": "stop_orbit",
+                    "type": "double",
+                    "value": "19451.0"
+                    }]
 
         #Number of linking events
         xband_linking_events = self.query_eboa.get_linking_events(gauge_names = {"filter": "SYSTEM_UNAVAILABILITY", "op": "=="}, 
@@ -210,7 +329,7 @@ class TestOpprip(unittest.TestCase):
                                                                     stop_filters = [{"date": "2020-11-26T23:00:00", "op": "=="}], 
                                                                     link_names = {"filter": "PLANNED_PLAYBACK", "op": "=="})
         
-        len(xband_linking_events["linking_events"]) == 2
+        assert len(xband_linking_events["linking_events"]) == 2
 
         ocp_linking_events = self.query_eboa.get_linking_events(gauge_names = {"filter": "SYSTEM_UNAVAILABILITY", "op": "=="}, 
                                                                     gauge_systems = {"filter": "S2B", "op": "=="}, 
@@ -218,7 +337,7 @@ class TestOpprip(unittest.TestCase):
                                                                     stop_filters = [{"date": "2020-11-26T23:00:00", "op": "=="}], 
                                                                     link_names = {"filter": "PLANNED_PLAYBACK", "op": "=="})
         
-        len(xband_linking_events["linking_events"]) == 2
+        assert len(ocp_linking_events["linking_events"]) == 2
 
         msi_linking_events = self.query_eboa.get_linking_events(gauge_names = {"filter": "SYSTEM_UNAVAILABILITY", "op": "=="}, 
                                                                     gauge_systems = {"filter": "S2B", "op": "=="}, 
@@ -226,7 +345,7 @@ class TestOpprip(unittest.TestCase):
                                                                     stop_filters = [{"date": "2020-11-26T16:30:00", "op": "=="}], 
                                                                     link_names = {"filter": "PLANNED_CUT_IMAGING", "op": "=="})
         
-        len(xband_linking_events["linking_events"]) == 5
+        assert len(msi_linking_events["linking_events"]) == 5
 
         mmfu_linking_events = self.query_eboa.get_linking_events(gauge_names = {"filter": "SYSTEM_UNAVAILABILITY", "op": "=="}, 
                                                                     gauge_systems = {"filter": "S2B", "op": "=="}, 
@@ -234,7 +353,8 @@ class TestOpprip(unittest.TestCase):
                                                                     stop_filters = [{"date": "2020-11-26T20:30:00", "op": "=="}], 
                                                                     link_names = {"filter": "PLANNED_CUT_IMAGING", "op": "=="})
         
-        len(xband_linking_events["linking_events"]) == 4
+        assert len(mmfu_linking_events["linking_events"]) == 4
+      
 
         
 
